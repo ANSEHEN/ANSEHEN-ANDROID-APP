54c8653eae83c4ab54066de8b987b922cd7c3837
sungkyun88 <tasung08@gmail.com>
2017-07-10 18:48:18 +0900

    Login_complete. need attach camera.

--- app/src/main/java/org/techtown/ansehen/RegisterActivity.java
+++ app/src/main/java/org/techtown/ansehen/RegisterActivity.java
@@ -1,11 +1,16 @@
 package org.techtown.ansehen;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import static android.content.ContentValues.TAG;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +21,14 @@ public class RegisterActivity extends AppCompatActivity {
     String loginName;
     String loginPw;
     String loginPhone;
+    String phoneNum;
+
+
+    String getPhoneNumber()
+    {
+        TelephonyManager mgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        return mgr.getLine1Number();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,13 +67,17 @@ public class RegisterActivity extends AppCompatActivity {
                         String name = nameText.getText().toString();
                         String password = pwText.getText().toString();
                         String inputPhone = phoneText.getText().toString();
-                        String phonenum = "010-7777-7777";
-                        //Log.e(TAG,"name : "+name);
-                        //Log.e(TAG,"passWord : "+password);
-                        //Log.e(TAG,"inputPhone : "+inputPhone);
+
+
+                        String phoneNum = getPhoneNumber();
+
+                        Log.e(TAG,"name : "+name);
+                        Log.e(TAG,"passWord : "+password);
+                        Log.e(TAG,"inputPhone : "+inputPhone);
+                        Log.e(TAG,"phoneNum : "+phoneNum);
 
                         httpClient http = new httpClient();
-                        http.HttpFileUpload(name,password,phonenum,inputPhone);
+                        http.HttpFileUpload(name,password,phoneNum,inputPhone);
                     }
                 }).start();
                 Intent registerIntent = new Intent(RegisterActivity.this, CameraActivity.class);
